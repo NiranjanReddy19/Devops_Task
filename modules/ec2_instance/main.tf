feec096be323cbcebe787d2193f054bb5e686b7b
G Niranjan Reddy Reddy <niranjan.reddy@celebaltech.com>
2026-05-18 10:56:16 +0530

    security Group rules updated

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "EC2_SG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["44.192.87.20/32"]
   }
 
     ingress {
